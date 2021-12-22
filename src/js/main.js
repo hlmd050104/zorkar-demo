d1be141abd8673b4dbabb5b10027df8624dd98ec
Hà Lâm Minh Duy <hlmd050104@gmail.com>
2021-12-22 11:05:29 +0700

    demo

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+import { header } from './header'
+import { slider } from './sliderShow'
+import { cart } from './cart'
+import { tab } from './tab'
+import { scrollTop } from './scrollTop'
+import { chart } from './chart'
+
+window.addEventListener('DOMContentLoaded', () => {
+  header()
+  slider()
+  cart()
+  tab()
+  scrollTop()
+  chart()
+})
